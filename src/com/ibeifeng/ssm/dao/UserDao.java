84ab72340e7c9e5b2861d6b8a7a1003f62e3d3ab
X230 <13961820245@qq.com>
2017-02-24 11:25:28 +0800

    首次提交！多多关照！

---
+++ src/com/ibeifeng/ssm/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.ibeifeng.ssm.dao;
+
+import com.ibeifeng.ssm.mapper.UserMapper;
+import com.ibeifeng.ssm.model.User;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by zmzy on 2017/2/23.
+ */
+
